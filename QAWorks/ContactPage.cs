0dca4439820ff66496769bca1ad5c19a01b9a085
MattAyoola <37045063+MattAyoola@users.noreply.github.com>
2018-03-05 13:04:41 +0000

    Initial commit

---
+++ QAWorks/ContactPage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QAWorks
+{
+    public class ContactPage
+    {
+        public IWebDriver driver;
+
+        public void LaunchBrowser(string baseUrl)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+
+        public void TearDownBrowser()
+        {
+            Console.WriteLine(driver.Url);
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        public string PageTitle()
+        {
+            return driver.Title;
+        }
+
+        public void NavigateToContactSession()
+        {
+           driver.FindElement(By.XPath("//*[@id='menu-item-18894']/a/span")).Click();
+        }
+
+        public void Name(string name)
+        {
+            driver.FindElement(By.XPath("//*[@id='qaworks-contact-us']/p[1]/label/span/input")).SendKeys(name);
+        }
+        public void Email(string email)
+        {
+            driver.FindElement(By.XPath("//*[@id='qaworks-contact-us']/p[2]/label/span/input")).SendKeys(email);
+        }
+        public void Subject(string subject)
+        {
+            driver.FindElement(By.XPath("//*[@id='qaworks-contact-us']/p[3]/label/span/input")).SendKeys(subject);
+        }
+        public void Message(string message)
+        {
+            driver.FindElement(By.XPath("//*[@id='qaworks-contact-us']/p[4]/label/span/textarea")).SendKeys(message);
+        }
+        public void Send()
+        {
+            driver.FindElement(By.Id("contact-us-send")).Click();
+        }
+    }
+}
---
+++ QAWorks/ContactUs.feature
@@ -0,0 +1,13 @@
+﻿Feature: ContactUsPage
+	As an end user
+	I want a contact us page
+	So that I can find out more about QAWorks exciting services
+
+# Acceptance Test/ Scenarios
+Scenario: Valid Submission
+	Given I am on the QAWorks Site
+	When I navigate to contact us section
+	Then I should be able to contact QAWorks with the following information
+	 	 | Name     | Email                | Subject         | Message                                   |
+	 	 | j.Bloggs | j.Bloggs@qaworks.com | test automation | please contact me I want to find out more |
+
